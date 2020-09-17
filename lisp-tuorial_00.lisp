43bdcbdaab32c85426be930d41c9208d315539f6
shubham pathak <95shubham1@gmail.com>
2020-09-17 16:13:23 +0530

    Introduction to lisp,variable type and print function

---
+++ lisp-tuorial_00.lisp
@@ -0,0 +1,15 @@
+; print statement
+(print "whats your name")
+
+;global variables->defvar remain constant until a new value if provided
+
+(defvar x 200)
+(print x)
+(write x)
+; have to check diif between print and write function in lisp
+
+; no type declaration variables can be direclty set using setq construct
+(setq x 20)
+(print x)
+
+
